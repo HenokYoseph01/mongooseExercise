f9e152dc2ed5381b4439719b9157266d7fee0d1f
Henok Yoseph <henok.y2014@gmail.com>
2022-04-19 08:53:12 +0300

    Creating inital server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("I love edu");
+//Declare http server
+const http = require("http");
+
+//Declare express
+const express = require("express");
+
+//create app
+const app = express();
+
+//create server
+const server = http.createServer(app);
+
+//create Port
+const port = process.env.PORT || 3000;
+
+//create listener
+server.listen(port,()=>{
+    console.log(`Server listening on port ${port}`);
+})
